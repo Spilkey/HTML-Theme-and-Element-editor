8c1cb856d7209bf2d04343470b72f415d7402bf5
Spilkey <spilkey1234@gmail.com>
2020-12-14 17:07:35 -0500

    initial commit for server

---
+++ server/server/app.py
@@ -0,0 +1,11 @@
+# my-app/server/server/app.py
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def getRoot():
+    return "Welcome!"
+
+@app.route("/app/", defaults={"path": "index.html"})
+@app.route("/app/<path:path>")
+def getApp(path):
+    return app.send_static_file(path)
