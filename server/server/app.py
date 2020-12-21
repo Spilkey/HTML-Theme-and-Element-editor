895327f82b62822a96c0268f5acdfa53f2b251b6
Spilkey <spilkey1234@gmail.com>
2020-12-20 21:51:29 -0500

    added randomized colour palette generator

--- server/server/app.py
+++ server/server/app.py
@@ -1,6 +1,17 @@
 # my-app/server/server/app.py
 from flask import Flask
+from flask_cors import CORS, cross_origin
+
+import seaborn as sns
+import math
+import numpy as np
+import random
+import requests
+
+
 app = Flask(__name__)
+cors = CORS(app)
+
 @app.route("/")
 def getRoot():
     return "Welcome!"
@@ -8,4 +19,15 @@ def getRoot():
 @app.route("/app/", defaults={"path": "index.html"})
 @app.route("/app/<path:path>")
 def getApp(path):
-    return app.send_static_file(path) 
+    return app.send_static_file(path)
+
+@app.route("/random-palette")
+def getRandomPalette():
+
+    url = "http://colormind.io/api/";
+    data = {
+	    'model' : "default",
+    }
+    response = requests.get(url, json=data)
+    results = {"colors": response.json()["result"]}
+    return results
